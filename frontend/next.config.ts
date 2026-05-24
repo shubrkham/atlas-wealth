8403aa03a4d80f21eaea9f1e541a54578eb3f19f
Shubham Karalkar <shubhamkaralkar123@gmail.com>
2026-05-25 04:23:21 +0530

    add clerk domain config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_CLERK_DOMAIN: process.env.NEXT_PUBLIC_CLERK_DOMAIN,
+  },
 };
 
 export default nextConfig;
